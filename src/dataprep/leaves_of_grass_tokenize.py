6656fa843f8541464791af396e1eeb5fbaa7a961
Zach OBrien <zachobrien99@gmail.com>
2022-12-06 23:35:52 -0500

    fix: tokenize_poem now only tokenizes, does not wrap with start/end symbols

--- src/dataprep/leaves_of_grass_tokenize.py
+++ src/dataprep/leaves_of_grass_tokenize.py
@@ -4,18 +4,14 @@ Functionality for preprocessing data to be used in n-gram language models
 """
 
 from nltk.tokenize import word_tokenize
-from nltk.util import pad_sequence
 
 
-def tokenize_poem(poem, n, newline_sym="<nl>", poem_start_sym="<p>", poem_end_sym="</p>"):
+def tokenize_poem(poem, newline_sym="<nl>"):
     """Convert a poem into tokens ready to be given to an `n`-gram language model
 
     Args:
         poem (str): A poem as a single string
-        n (int): The `n` in n-gram. If this is 2, the function makes 2-grams
         newline_sym (str): Symbol to replace newline characters with
-        poem_start_sym (str): Symbol to pad at the start of the poem
-        poem_end_sym (str): Symbol to pad at the end of the poem
     
     Returns:
         list[str]: A list of tokens
@@ -24,12 +20,4 @@ def tokenize_poem(poem, n, newline_sym="<nl>", poem_start_sym="<p>", poem_end_sy
     poem = poem.replace("\n", "NEWLINE")
     poem = word_tokenize(poem)
     poem = [word if not word == "NEWLINE" else newline_sym for word in poem]
-    poem = list(pad_sequence(
-        poem,
-        pad_left=True, 
-        left_pad_symbol=poem_start_sym,
-        pad_right=True,
-        right_pad_symbol=poem_end_sym,
-        n=n
-    ))
     return poem
--- src/dataprep/test_leaves_of_grass_tokenize.py
+++ src/dataprep/test_leaves_of_grass_tokenize.py
@@ -9,6 +9,6 @@ from leaves_of_grass_tokenize import tokenize_poem
 class TestTokenizePoem:
     def test_tokenize_poem(self):
         poem = "One's-self I sing. A simple \n separate person."
-        expected = ["<p>", "<p>", "one's-self", "i", "sing", ".", "a", "simple", "<nl>", "separate", "person", ".", "</p>", "</p>"]
-        actual = tokenize_poem(poem, 3, "<nl>", "<p>", "</p>")
+        expected = ["one's-self", "i", "sing", ".", "a", "simple", "<nl>", "separate", "person", "."]
+        actual = tokenize_poem(poem, "<nl>")
         assert actual == expected
